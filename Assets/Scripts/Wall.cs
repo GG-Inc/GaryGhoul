165ec1742c2544e239bec5e271b9229c0c160b35
Dawson Conway <dconman02@gmail.com>
2017-02-22 21:49:17 -0600

    Added Wall generation

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+	const float DOOR_WIDTH = 0.2f;
+	const float DOOR_HEIGHT = 0.8f;
+	public float doorPosition = 0.0f;
+
+	public void config(float doorPos) {
+		GameObject[] components = new GameObject[3];
+		for (int i = 0; i < 3; i++) {
+			components [i] = GameObject.CreatePrimitive (PrimitiveType.Plane);
+			components [i].transform.SetParent(this.transform,false);
+		}
+		components [0].transform.localScale = new Vector3 ( doorPos, 1, DOOR_HEIGHT);
+		components [0].transform.localPosition = new Vector3 ( doorPos/2 , 0, -DOOR_HEIGHT/2) * 10;
+		components [1].transform.localScale = new Vector3 ( 1 - (doorPos + DOOR_WIDTH), 1, DOOR_HEIGHT);
+		components [1].transform.localPosition = new Vector3 ( (1 + doorPos + DOOR_WIDTH)/2 , 0, -DOOR_HEIGHT/2) * 10;
+		components [2].transform.localScale = new Vector3 ( 1, 1, 1 - DOOR_HEIGHT);
+		components [2].transform.localPosition = new Vector3 ( .5f , 0, -(1+DOOR_HEIGHT)/2) * 10;
+	}
+
+	// Use this for initialization
+	void Start () {
+		config (doorPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
