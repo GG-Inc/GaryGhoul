5f16778fcb4d8ac0e167db2e048c7d51fe03b032
Dawson Conway <dconman02@gmail.com>
2017-03-04 10:42:31 -0600

    cleaned up wall/room code and improved it

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -7,24 +7,29 @@ public class Room : MonoBehaviour {
 	public float east = 0f;
 	public float south = 0f;
 	public float west = 0f;
+	private Vector3 NORTH_POS = new Vector3(-2.5f,0f,2.5f);
+
+	enum Direction {
+		N = 0,
+		E = 90,
+		S = 180,
+		W = 270
+	}
 
 	public void config (float n, float e, float s, float w) {
-		Wall[] walls = GetComponentsInChildren<Wall> ();
-		foreach (Wall wall in walls) {
-			switch (wall.dir) {
-			case Wall.Direction.N:
-				wall.config (n);
-				break;
-			case Wall.Direction.E:
-				wall.config (e);
-				break;
-			case Wall.Direction.S:
-				wall.config (s);
-				break;
-			case Wall.Direction.W:
-				wall.config (w);
-				break;
-			}
+		KeyValuePair<Direction, float>[] directions = {
+			new KeyValuePair<Direction, float>( Direction.N, n ),
+			new KeyValuePair<Direction, float>( Direction.E, e ),
+			new KeyValuePair<Direction, float>( Direction.S, s ),
+			new KeyValuePair<Direction, float>( Direction.W, w )
+		};
+
+		foreach (KeyValuePair<Direction, float> dir in directions) {
+			GameObject child = new GameObject ();
+			positionChild (child.transform, dir.Key);
+			Wall wall = child.AddComponent<Wall> ();
+			wall.config (dir.Value);
+
 		}
 	}
 
@@ -36,5 +41,13 @@ public class Room : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void positionChild(Transform tr, Direction dir){
+		tr.SetParent(transform,false);
+		tr.localPosition = NORTH_POS;
+		tr.localScale = new Vector3 (0.5f, -1, 0.25f);
+		tr.Rotate(90, 0, 0);
+		tr.RotateAround (transform.position, Vector3.up, (float) dir);
 	}
 }
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -5,35 +5,15 @@ using UnityEngine;
 public class Wall : MonoBehaviour {
 	const float DOOR_WIDTH = 0.2f;
 	const float DOOR_HEIGHT = 0.8f;
-	public Direction dir;
-
-	public enum Direction {
-		N,
-		E,
-		S,
-		W
-	}
 
 	public void config(float doorPos) {
 		if (doorPos <= 0) {
 			return;
 		}
-		if (doorPos + DOOR_WIDTH > 1) {
+		if (doorPos + DOOR_WIDTH >= 1) {
 			doorPos = 1;
 		}
-		GameObject[] components = new GameObject[3];
-		for (int i = 0; i < 3; i++) {
-			components [i] = GameObject.CreatePrimitive (PrimitiveType.Plane);
-			components [i].transform.SetParent(this.transform,false);
-		}
-		components [0].transform.localScale = new Vector3 ( doorPos, 1, DOOR_HEIGHT);
-		components [0].transform.localPosition = new Vector3 ( doorPos/2 , 0, -DOOR_HEIGHT/2) * 10;
-		if (doorPos < 1) {
-			components [1].transform.localScale = new Vector3 (1 - (doorPos + DOOR_WIDTH), 1, DOOR_HEIGHT);
-			components [1].transform.localPosition = new Vector3 ((1 + doorPos + DOOR_WIDTH) / 2, 0, -DOOR_HEIGHT / 2) * 10;
-		}
-		components [2].transform.localScale = new Vector3 (1, 1, 1 - DOOR_HEIGHT);
-		components [2].transform.localPosition = new Vector3 (.5f, 0, -(1 + DOOR_HEIGHT) / 2) * 10;
+		setChildren (doorPos);
 	}
 
 	// Use this for initialization
@@ -43,5 +23,21 @@ public class Wall : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	private void setChildren(float doorPos)
+	{
+		GameObject[] components = new GameObject[3];
+		for (int i = 0; i < (doorPos < 1 ? 3 : 2) ; i++) {
+			components [i] = GameObject.CreatePrimitive (PrimitiveType.Plane);
+			components [i].transform.SetParent (this.transform, false);
+		}
+		components [0].transform.localScale = new Vector3 (doorPos, 1, DOOR_HEIGHT);
+		components [0].transform.localPosition = new Vector3 (doorPos / 2, 0, -DOOR_HEIGHT / 2) * 10;
+		components [1].transform.localScale = new Vector3 (1, 1, 1 - DOOR_HEIGHT);
+		components [1].transform.localPosition = new Vector3 (.5f, 0, -(1 + DOOR_HEIGHT) / 2) * 10;
+		if (doorPos < 1) {
+			components [2].transform.localScale = new Vector3 (1 - (doorPos + DOOR_WIDTH), 1, DOOR_HEIGHT);
+			components [2].transform.localPosition = new Vector3 ((1 + doorPos + DOOR_WIDTH) / 2, 0, -DOOR_HEIGHT / 2) * 10;
+		}
 	}
 }
